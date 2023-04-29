3e15cfbfb974b3680088d1ab325481778e2de712
Paulo Victor Fabrício <87426891+plfbc@users.noreply.github.com>
2023-04-29 14:46:38 -0300

    UPDATE CLIENT SOCKET

--- TCP/Socket_Client_TCP.py
+++ TCP/Socket_Client_TCP.py
@@ -10,18 +10,12 @@ interface_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 interface_s.connect((host,port))
 
 
-
-
-
 #---------------------------------#
 while True:
     pot = random.randint(0,29)
-
     choice = str(random.randint(10**pot, (10**(pot+1))-1))
 
-
-
-    teste = input("Manda: ")
+    teste = input("Digite o comando SAIR caso queira sair, caso contrário,\na próxima string será enviada: ")
     if(teste.upper() == "SAIR"):
         print("fechando conexão com o servidor")
         interface_s.sendall(str.encode(teste))
