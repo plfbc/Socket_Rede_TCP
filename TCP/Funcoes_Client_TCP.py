618fde9d50510b92aaf9accb6a3098576778ec74
Pedro Karin <79324892+Karin-Pedro@users.noreply.github.com>
2023-04-30 11:17:33 -0300

    Update Funcoes_Client_TCP.py

--- TCP/Funcoes_Client_TCP.py
+++ TCP/Funcoes_Client_TCP.py
@@ -1,5 +1,9 @@
 import random
 
+# A função abaixo escolhe qualquer número entre 0 e o maior número de 30 casas decimais.
+# Seu funcionamento é dividio em: 1 - Sorteio do número de casas decimais do número 2 - Sorteio do número em sí a partir da potência
+# O retorno da função é o número em string.
+
 def geracao_n() -> str:
     pot = random.randint(0,29)
     choice = str(random.randint(10**pot, (10**(pot+1))-1))
