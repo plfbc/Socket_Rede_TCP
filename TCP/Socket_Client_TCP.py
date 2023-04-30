af634852c9fe8c9be6f2d66eb60c5195a822bb38
Pedro Karin <79324892+Karin-Pedro@users.noreply.github.com>
2023-04-30 11:25:30 -0300

    Update Socket_Client_TCP.py

--- TCP/Socket_Client_TCP.py
+++ TCP/Socket_Client_TCP.py
@@ -9,8 +9,8 @@ while True:
     interface_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     interface_s.connect((host,port))
-#---------------------------------#
-
+#--------------------------------------------------------------------#
+# Essa fração do código é responsável por interagir com o servidor, enviando mensagens e recebendo
     msg_cliente = FCT.geracao_n()
 
     interface_s.sendall(str.encode(msg_cliente))
@@ -19,7 +19,8 @@ while True:
     print(f'string original: {msg_cliente}')
     print('Mensagem do servidor: ', data.decode())
     
-
+#-------------------------------------------------------------------#
+# A fim de criar o tempo entre ciclos de interação, essa fração do código estabelece 10s de espera.
     for i in range(10):
         sleep(1)
         print(f"\nesperando recomeço de ciclo, passou {i+1} segundos")
